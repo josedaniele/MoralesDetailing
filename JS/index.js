4bf23a017c7dfa6976f22b5c9c54f95e02a576d5
josedaniele <josedaniele493@gmail.com>
2021-12-30 23:39:26 -0300

    vs 0.3

--- JS/index.js
+++ JS/index.js
@@ -1,45 +1,79 @@
-let entrada = prompt("Escirba el numero en base al lavado que quiera: \n" + "1. Automovil \n" + "2. Camioneta \n" + "3.Motocicleta \n" + "4. Automovil con lavado de interior \n" + "5. Camioneta con lavado de interior \n")
-let efectivoTarjeta = prompt("Si va a paga en efetivo escriba E si va a pagar con tarjeta escriba T \n" + "El pago con tarjeta tiene un incremento del 10% sobre el precio del lavado \n" + "Respete el formato mayuscula para las letras")
+//Captura de datos del usuario
+function enviar () {
+    const select1 = document.getElementById("tipoLavado");
+    const select2 = document.getElementById("medioPago");
 
-function lavado() {
     
-    switch  (entrada){
-        case "1":
-            return 700;
-           break;
-        case "2":
-             return 1000; 
-           break;
-        case "3":
-            return 400;
-           break;
-        case "4":
-             return 1000;
-        case "5":
-             return 1300;
-           break;
-        default : 
-            return alert("numero ingresado invalido");
-            break;
-        
-    }
-                
-}
-function paga(){
-    switch(efectivoTarjeta){
-        case "T":
-            return 1.1;
-            break;
-        case "E":
-            return 1;
-            break;
-        default:
-            alert("letra ingresada invalida");
-            break;
-            }
-
+// Devolucion de valores para el tipo de lavado y el medio de pago que elija el usuario 
+    function lavado() {
+    
+        switch  (select1.value){
+            case "1":
+                return 700;
+               break;
+            case "2":
+                 return 1000; 
+               break;
+            case "3":
+                return 400;
+               break;
+            case "4":
+                 return 1000;
+            case "5":
+                 return 1300;
+               break;
+            default : 
+                return console.log("numero ingresado invalido");
+                break;
             
+        }
+                    
     }
-    const IVA = x => x*1.21
+    function paga(){
+        switch(select2.value){
+            case "T":
+                return 1.1;
+                break;
+            case "E":
+                return 1;
+                break;
+            default:
+                console.log("letra ingresada invalida");
+                break;
+                }
+    
+                
+        }
+        const IVA = x => x*1.21
+    //Muestra al usuario su seleccion
+    let precioFinal =alert("Su lavado tiene un coste final de $" + IVA(lavado())*paga());
+}
 
-let precioFinal =alert("Su lavado tiene un coste final de $" + IVA(lavado())*paga());
+class Cliente {
+    constructor(nombre , apellido , email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+ 
+}
+function enviando () {
+    const inputName = document.getElementById("name");
+    const inputLastName = document.getElementById("lastname");
+    const inputEmail = document.getElementById("email");
+
+    const capturaDatos = new Cliente (inputName.value, inputLastName.value, inputEmail.value)
+    
+    const clientes = []
+    
+    function mostrarDatos(){
+        clientes.push(capturaDatos);
+        console.log(clientes);
+    }
+    mostrarDatos()
+    
+}
+
+
+
+
